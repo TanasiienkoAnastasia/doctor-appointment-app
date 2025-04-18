0a248d76b1381db5b08dd0518e766dde51537e83
Anastasiia <tanasiienkoanastasia@gmail.com>
2025-04-18 17:47:16 +0300

    Remove notification from db

--- appointment-service/app/models/__init__.py
+++ appointment-service/app/models/__init__.py
@@ -1,13 +1,11 @@
 from .user import User
 from .appointment import Appointment
 from .medical_record import MedicalRecord
-from .notification import Notification
 from .doctor_schedule import DoctorSchedule
 
 __all__ = [
     "User",
     "Appointment",
     "MedicalRecord",
-    "Notification",
     "DoctorSchedule"
 ]
--- appointment-service/app/models/notification.py
+++
@@ -1,7 +0,0 @@
-from app.extensions import db
-
-class Notification(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    recipient = db.Column(db.String(120), nullable=False)
-    message = db.Column(db.String(500), nullable=False)
-    sent_at = db.Column(db.DateTime, nullable=False)
