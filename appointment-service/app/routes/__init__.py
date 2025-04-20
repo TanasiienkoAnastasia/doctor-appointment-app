36b10df06135a3a1904b0a3f5082f01714e806c6
Anastasiia <tanasiienkoanastasia@gmail.com>
2025-04-21 02:56:04 +0300

    Fix routes import

--- appointment-service/app/routes/__init__.py
+++ appointment-service/app/routes/__init__.py
@@ -1,6 +1,8 @@
 from .auth_routes import auth_routes
 from .user_routes import user_routes
 from .patient_appointments_routes import patient_appointments_routes
+from .recommendation_routes import recommendation_routes
+from .doctor_routes import doctor_routes
 
 __all__ = [
     'auth_routes',
